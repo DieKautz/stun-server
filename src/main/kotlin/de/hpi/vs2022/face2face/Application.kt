5be434b65cd5eed4f334d6eae317c486641434cb
Malte Stellmacher <malte.stellmacher@student.hpi.uni-potsdam.de>
2022-06-19 18:54:57 +0200

    simple server listening for udp

--- src/main/kotlin/de/hpi/vs2022/face2face/Application.kt
+++ src/main/kotlin/de/hpi/vs2022/face2face/Application.kt
@@ -1,11 +1,25 @@
 package de.hpi.vs2022.face2face
 
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import de.hpi.vs2022.face2face.plugins.*
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import org.slf4j.LoggerFactory
+
+val LOG = LoggerFactory.getLogger("ktor.application")
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        configureSockets()
-    }.start(wait = true)
+
+    val host = System.getenv().getOrDefault("PORT", "127.0.0.1")
+    val port = System.getenv().getOrDefault("PORT", "3478").toInt()
+    runBlocking {
+        val serverSocket  = aSocket(SelectorManager(Dispatchers.IO))
+            .udp()
+            .bind(InetSocketAddress(host, port))
+        println("Listening on ${serverSocket.localAddress}")
+        while (true) {
+            val datagram = serverSocket.incoming.receive()
+            LOG.info("datagram received: ${datagram.address}, ${datagram.packet.readText()}")
+        }
+    }
 }
