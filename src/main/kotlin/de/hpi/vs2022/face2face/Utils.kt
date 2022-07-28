c8e75e8f960be0d9d84fd884e121b9d83de59764
Malte Stellmacher <malte.stellmacher@student.hpi.uni-potsdam.de>
2022-07-28 17:59:29 +0200

    add utility functions to test

--- src/main/kotlin/de/hpi/vs2022/face2face/Utils.kt
+++ src/main/kotlin/de/hpi/vs2022/face2face/Utils.kt
@@ -1,23 +1,13 @@
 package de.hpi.vs2022.face2face
 
-import io.ktor.utils.io.bits.*
 import kotlin.experimental.xor
 
 fun ByteArray.toHex(): String = joinToString(separator = "") { eachByte -> "%02x".format(eachByte) }
-
-fun ByteArray.toUInt(): UInt {
-    var result = 0u
-    for (i in this.indices) {
-        result = result or (this[i].toUInt() shl 8 * i)
-    }
-    return result
-}
 
 fun ByteArray.xor(other: ByteArray): ByteArray = mapIndexed { index: Int, byte: Byte ->
     return@mapIndexed byte xor other[index]
 }.toByteArray()
 
-fun Short.toByteArray() = byteArrayOf(highByte, lowByte)
 
 fun Int.toByteArray() = byteArrayOf(
     (this shr 24).toByte(),
--- src/test/kotlin/de/hpi/vs2022/face2face/Utils.kt
+++ src/test/kotlin/de/hpi/vs2022/face2face/Utils.kt
@@ -2,10 +2,23 @@ package de.hpi.vs2022.face2face
 
 import de.hpi.vs2022.face2face.stun.Message
 import org.jetbrains.annotations.TestOnly
+import kotlin.experimental.xor
 import kotlin.random.Random
 
 @TestOnly
 internal fun createRandomMessageNoAttr() = Message(
     Message.Type.values().random(),
     Random.nextBytes(12)
+)
+
+fun ByteArray.xor(other: ByteArray): ByteArray = mapIndexed { index: Int, byte: Byte ->
+    return@mapIndexed byte xor other[index]
+}.toByteArray()
+
+
+fun Int.toByteArray() = byteArrayOf(
+    (this shr 24).toByte(),
+    (this shr 16).toByte(),
+    (this shr 8).toByte(),
+    (this shr 0).toByte(),
 )
